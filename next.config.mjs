db04aeb3cd04312d9fa0f9333c3628d23f795a8a
Shane Friedman <smoores-gpg@friedmans.us>
2025-01-04 18:36:08 -0500

    Add payload

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,4 +6,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPayload(nextConfig)
